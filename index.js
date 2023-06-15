7659b9577a04397cf71e12bcf67aa47bd9c0d7b0
Denis mulwa <marshmellowking99@gmail.com>
2023-06-15 17:44:20 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    let newResult  = record.find(results => results.result == 'W')
+    if(newResult == undefined) {
+        return newResult
+    }else{
+        return newResult.year;
+    }
+}
